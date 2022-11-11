47a3a58240a13d6e4055e49073dc1eaf906206a8
Pranav Sakthivel <33013003+PranavSakthivel@users.noreply.github.com>
2022-11-10 19:08:09 -0800

    Added assets and placeholders for information

---
+++ fun-xyz/src/constants/index.js
@@ -0,0 +1,52 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, fun, binance, coinbase, dropbox, send, shield, star, email  } from "../assets";
+
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "about",
+        title: "About"
+    },
+    {
+        id: "mission",
+        title: "Mission",
+    },
+    {
+        id: "team",
+        title: "Team",
+    },
+    {
+        id: "careers",
+        title: "Careers",
+        link: "https://jobs.lever.co/fun.xyz"
+    },
+];
+
+export const socials = [
+    {
+        id: "twitter",
+        icon: twitter,
+        link: "https://twitter.com/fun"
+    },
+    { 
+        id: "linkedin",
+        icon: linkedin,
+        link: "https://www.linkedin.com/company/funxyz"
+    },
+    {
+        id: "email",
+        icon: email,
+        link: "mailto:hi@fun.xyz"
+    }
+]
+
+export const footer = "© 2022 The Fun Group, Inc.";
+export const partners = [
+    {
+        id: "odsy",
+        name: "Odsy Network",
+        link: "http://odsy.xyz/"
+    }
+];
