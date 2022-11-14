40293288a788afb6138d3ab06546b95bab75b82b
pranavsakthivel <{ID}+{username}@users.noreply.github.com>
2022-11-14 14:12:35 -0800

    Updated Home and Features/Learn More

--- fun-xyz/src/components/index.js
+++ fun-xyz/src/components/index.js
@@ -3,11 +3,15 @@ import Home from "./Home";
 import Mission from "./Mission";
 import Navbar from "./Navbar";
 import Team from "./Team";
+import LearnMore from "./LearnMore";
+import Button from "./Button";
 
 export {
     About,
     Home,
     Mission,
     Navbar,
-    Team
-}
+    Team,
+    LearnMore,
+    Button
+};
