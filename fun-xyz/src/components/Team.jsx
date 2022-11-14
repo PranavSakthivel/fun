f0376d52c88f9557540437cb58b595e44fdce2b1
pranavsakthivel <{ID}+{username}@users.noreply.github.com>
2022-11-14 15:40:12 -0800

    Added team section

--- fun-xyz/src/components/Team.jsx
+++ fun-xyz/src/components/Team.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { team } from '../constants';
+import styles from '../styles';
+import MemberCard from './MemberCard';
 
-const Team = () => {
-  return (
-    <div>Team</div>
-  )
-}
+const Team = () => (
+  <section id='members' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+
+    <div className='absolute z-[0] w-[60%] h-[60%] -left-[1%] rounded-full blue__gradient'/>
+
+    <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+      <h1 className={styles.heading2}>Meet The Team</h1>
+    </div>
+
+    <div className='flex flex-wrap sm:justify-start justify-center w-full team-container relative z-[1]'>
+      { team.map((card) => (
+        <MemberCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+)
 
 export default Team
